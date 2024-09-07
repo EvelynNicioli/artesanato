4d2cdad01c1772747ad7900be326e25c9946eed1
EvelynNicioli <evelynn.lyn@gmail.com>
2024-09-07 15:45:28 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Amigurumi",
+        descricao: "O Amigurumi é a arte japonesa de criar bonecos e bichinhos de pelúcia através do crochê ou tricô. São peças personalizadas e encantadoras, perfeitas para presentear ou decorar.",
+        link: "https://www.seusitedeamigurumi.com",
+        tags:"amigurumi amigumi"
+    },
+    {
+        titulo: "Crochê",
+        descricao: "O crochê é uma técnica milenar de tecelagem manual que utiliza um único gancho para criar peças diversas, como roupas, acessórios e objetos de decoração.",
+        link: "https://escoladeartesmanuais.com.br/blog/a-historia-do-croche",
+        tags:"croche croxe"
+    },
+    {
+        titulo: "Macramê",
+        descricao: "O macramê é a arte de criar peças através de nós em cordas, fios ou barbantes. É uma técnica versátil que permite criar desde objetos decorativos até acessórios de moda.",
+        link: "https://www.seusitedemacrame.com",
+        tags:"macrame"
+    }
+];
